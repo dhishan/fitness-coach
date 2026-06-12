960bc67b426d54f5879bd25dcc5ed89e4af1fa0a
Dhishan Amaranath <iamdhishan@gmail.com>
2026-06-12 13:03:16 -0700

    fix: add ascending user_id+date index for week-range dashboard queries

--- terraform/main/indexes_workouts.tf
+++ terraform/main/indexes_workouts.tf
@@ -12,6 +12,20 @@ resource "google_firestore_index" "workouts_user_date" {
   }
 }
 
+resource "google_firestore_index" "workouts_user_date_asc" {
+  project    = var.project_id
+  database   = google_firestore_database.db.name
+  collection = "workouts"
+  fields {
+    field_path = "user_id"
+    order      = "ASCENDING"
+  }
+  fields {
+    field_path = "date"
+    order      = "ASCENDING"
+  }
+}
+
 resource "google_firestore_index" "workouts_user_exercise_date" {
   project    = var.project_id
   database   = google_firestore_database.db.name
